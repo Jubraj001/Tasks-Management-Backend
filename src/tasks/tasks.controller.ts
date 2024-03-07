803726e9b9afbf83040cfa9d1701a2a2b53dfc4f
Jubraj001 <jubrajdev001@gmail.com>
2024-03-08 00:50:53 +0530

    feat: basic functioning of the controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
-@Controller('tasks')
+@Controller('tasks') // Define the endpoint which the controller will handle
 export class TasksController {
   constructor(private tasksService: TasksService) {}
+
+  @Get() // Define the method(GET, POST, PUT, DELETE) and route which this method will handle
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  // Good practice to set it private
+  private tasks = [];
+
+  // Public by default
+  getAllTasks() {
+    return this.tasks;
+  }
+}
