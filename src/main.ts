d7876b75002d99c3d75408efb58578f6492f3408
Jubraj001 <jubrajdev001@gmail.com>
2024-03-08 12:31:42 +0530

    feat: Added ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe()); // Whenever NestJs encounters any of the validation decorators, it will execute validation Pipes
+
   await app.listen(3000);
 }
 bootstrap();
