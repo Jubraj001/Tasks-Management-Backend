4b23960a5cbc40cba16283050acaec827dc4556e
Jubraj001 <jubrajdev001@gmail.com>
2024-03-10 00:03:26 +0530

    feat: JWT strategy, GetUser decorator and Guard tasks controller

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,27 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserRepository } from './user.repository';
+import { JwtPayload } from './jwt-payload.interface';
+import { User } from './user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly userRepository: UserRepository) {
+    super({
+      secretOrKey: 'TOP_SECRET',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const { username } = payload;
+    const user: User = await this.userRepository.findOneBy({ username });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user; // Passport will inject it into the request object of our controller
+  }
+}
