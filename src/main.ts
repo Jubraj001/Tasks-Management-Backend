07951a362a15dcbbdb87d646fc6a89d6821ab748
Jubraj001 <jubrajdev001@gmail.com>
2024-03-10 19:24:51 +0530

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { TransformInterceptor } from './transform.interceptor';
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe()); // Whenever NestJs encounters any of the validation decorators, it will execute validation Pipes
   app.useGlobalInterceptors(new TransformInterceptor());
 
