7e68c0b83e1d595a222a8039dd26e1106576f9cf
Jubraj001 <jubrajdev001@gmail.com>
2024-03-10 18:41:56 +0530

    feat: config for dev, staging and prod for DB creds

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,31 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`], // dev, staging, prod
+    }),
     TasksModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'task-management',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      // Async because we need the config module to finish initialization before we are able to use dependency injection
+      imports: [ConfigModule], // This means that it depends on the ConfigModule so it knows to wait
+      inject: [ConfigService], // We want to inject the ConfigService
+      useFactory: async (configService: ConfigService) => {
+        // Async function which nest js calls, whatever is returned from this will be the config for the module
+        return {
+          type: 'postgres',
+          autoLoadEntities: true,
+          synchronize: true,
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+        };
+      },
     }),
     AuthModule,
   ],
